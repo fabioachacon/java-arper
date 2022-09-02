2135d7c2879ac9f08a4bbb0f61aab57e139de2a5
Fabio <fabioac71@gmail.com>
2022-09-02 12:42:31 -0300

    Changes

--- src/main/java/com/pkt/App.java
+++ src/main/java/com/pkt/App.java
@@ -5,8 +5,8 @@ import com.pkt.sniff.Arper;
 public class App {
 
     public static void main(String[] args) {
-        final String TARGET_IP = "";
-        final String GATEWAY_IP = "";
+        final String TARGET_IP = args[0];
+        final String GATEWAY_IP = args[1];
 
         Arper arper = new Arper(TARGET_IP, GATEWAY_IP);
         arper.run();
